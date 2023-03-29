ea8a0fe0251683f185d4c166ba0893abaa7eac29
Xexxxed <bukhariahmed76@gmail.com>
2023-03-29 08:43:06 -0230

    deathAnimationFix
    -Fixed Death animation
    -added delay to play sound

--- The Midnight Labyrinth/Assets/Scripts/PlayerHealth.cs
+++ The Midnight Labyrinth/Assets/Scripts/PlayerHealth.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class PlayerHealth : MonoBehaviour
 {
     public int health;
     public int maxHealth = 3;
     public AudioSource audioPlayer;
+    private float delayTime = 2.0f;
+    private float timeElapsed;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,16 +17,37 @@ public class PlayerHealth : MonoBehaviour
     }
 
     // Update is called once per frame
+    void changeScene()
+    {
+        SceneManager.LoadScene("Scene0");
+        Destroy(gameObject);
+    }
     public void TakeDamage(int amount)
     {
         health -= amount;
         if(health <= 0)
         {
-            Destroy(gameObject);
+            
             audioPlayer.Play();
+            Invoke("changeScene", 1.0f);
+            
+
 
         }
-        
+
 
     }
+    
+    //private void Update()
+    //{
+    //    if(GameObject.FindGameObjectsWithTag("Player")==null)
+    //    {
+    //        Debug.Log("In statement");
+    //        timeElapsed += Time.deltaTime;
+    //        if(timeElapsed > delayTime) 
+    //        {
+    //            ;
+    //        }
+    //    }
+    //}
 }
