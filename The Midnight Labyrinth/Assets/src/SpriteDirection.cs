2cfc7e610cdbc0dade09cb16accc06778fa99e2e
Daayim Asim <daayimasim81@gmail.com>
2023-04-01 20:11:18 -0230

    New Enemy Sprite / Animation
    - Changed Enemy Sprite
    - Added Enemy animation
    - Change Darkness Mask order layer to 1 from 0
    - Enemy is now on order layer 0

--- The Midnight Labyrinth/Assets/src/SpriteDirection.cs
+++ The Midnight Labyrinth/Assets/src/SpriteDirection.cs
@@ -8,13 +8,25 @@ public class SpriteDirection : MonoBehaviour
     public AIPath aiPath;
     private SpriteRenderer spriteRenderer;
 
+    Animator enemyanimator;
+
     private void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        enemyanimator = GetComponent<Animator>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
+        if (aiPath.desiredVelocity.x != 0.00f || aiPath.desiredVelocity.y != 0.00f){
+            enemyanimator.SetBool("isTracking", true);
+        }
+        else{
+            enemyanimator.SetBool("isTracking", false);
+            }
+
+        
+        
         if (aiPath.desiredVelocity.x >= 0.01f)
         {
             spriteRenderer.flipX = false;
